e35d546db834543a7a6ca970995e75734a5d9b6c
Dawid Grajewski <dawids115@gmail.com>
2018-05-10 17:32:01 +0200

    Dodanie "Super dobrej sztucznej inteligencji dawida" xD;

---
+++ NeuralNetworkCar/Assets/Scripts/CarController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public abstract class CarController : MonoBehaviour
+{
+    [Header("Steering")]
+    public int MovementSpeed;
+    public int RotationSpeed;
+
+    [Header("Sensors")]
+    public float SensorLength;
+    public float SensorAngle;
+
+    public float DistanceToLeftWall { get; private set; }
+    public float DistanceToRightWall { get; private set; }
+
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+        getDistanceToWalls();
+    }
+
+    public void MoveForward()
+    {
+        transform.position += transform.forward * MovementSpeed * Time.deltaTime;
+    }
+
+    public void TurnLeft()
+    {
+        transform.Rotate(Vector3.down * Time.deltaTime * RotationSpeed);
+    }
+
+    public void TurnRight()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * RotationSpeed);
+    }
+
+    private void getDistanceToWalls()
+    {
+        RaycastHit leftHit;
+        RaycastHit rightHit;
+        Vector3 leftSensorStartPosition = transform.position;
+        Vector3 rightSensorStartPosition = transform.position;
+
+        if (Physics.Raycast(leftSensorStartPosition, Quaternion.AngleAxis(-SensorAngle, transform.up) * transform.forward, out leftHit, SensorLength))
+        {
+            DistanceToLeftWall = Vector3.Distance(leftSensorStartPosition, leftHit.point);
+            Debug.Log("Left: " + DistanceToLeftWall);
+        }
+
+        if (Physics.Raycast(rightSensorStartPosition, Quaternion.AngleAxis(SensorAngle, transform.up) * transform.forward, out rightHit, SensorLength))
+        {
+            DistanceToRightWall = Vector3.Distance(rightSensorStartPosition, rightHit.point);
+            Debug.Log("Right: " + DistanceToRightWall);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
--- NeuralNetworkCar/Assets/Scripts/ManualCarController.cs
+++ NeuralNetworkCar/Assets/Scripts/ManualCarController.cs
@@ -3,72 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ManualCarController : MonoBehaviour
+public class ManualCarController : CarController
 {
     [Header("Steering")]
     public KeyCode UpKey = KeyCode.W;
     public KeyCode RightKey = KeyCode.D;
     public KeyCode LeftKey = KeyCode.A;
 
-    public int MovementSpeed = 15;
-    public int RotationSpeed = 90;
-
-    [Header("Sensors")]
-    public float SensorLength;
-    public float SensorAngle;
-
-
-    public float DistanceToLeftWall { get; private set; }
-    public float DistanceToRightWall { get; private set; }
-
-
     // Update is called once per frame
-    void Update ()
+    protected override void Update ()
     {
+        base.Update();
         move();
-
-        getDistanceToWalls();
     }
 
     private void move()
     {
         if (Input.GetKey(UpKey))
         {
-            transform.position += transform.forward * MovementSpeed * Time.deltaTime;
+            MoveForward();
         }
 
         if (Input.GetKey(RightKey))
         {
-            transform.Rotate(Vector3.up * Time.deltaTime * RotationSpeed);
+            TurnRight();
         }
         if (Input.GetKey(LeftKey))
         {
-            transform.Rotate(Vector3.down * Time.deltaTime * RotationSpeed);
+            TurnLeft();
         }
     }
 
-    private void getDistanceToWalls()
-    {
-        RaycastHit leftHit;
-        RaycastHit rightHit;
-        Vector3 leftSensorStartPosition = transform.position;
-        Vector3 rightSensorStartPosition = transform.position;
+    
 
-        if(Physics.Raycast(leftSensorStartPosition, Quaternion.AngleAxis(-SensorAngle, transform.up) * transform.forward, out leftHit, SensorLength))
-        {
-            DistanceToLeftWall = Vector3.Distance(leftSensorStartPosition, leftHit.point);
-            Debug.Log("Left: " + DistanceToLeftWall);    
-        }
-
-        if (Physics.Raycast(rightSensorStartPosition, Quaternion.AngleAxis(SensorAngle, transform.up) * transform.forward, out rightHit, SensorLength))
-        {
-            DistanceToRightWall = Vector3.Distance(rightSensorStartPosition, rightHit.point);
-            Debug.Log("Right: " + DistanceToRightWall); 
-        }
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
+    
 }
