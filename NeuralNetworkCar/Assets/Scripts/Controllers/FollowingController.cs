1c384821834cc6f5476a452e399c02728e2fabee
Dawid Grajewski <dgrajewski@evertop.pl>
2018-12-27 16:12:13 +0100

    Znaczne powiększenie mapy; Kamera przesuwająca się za autkiem;

---
+++ NeuralNetworkCar/Assets/Scripts/Controllers/FollowingController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class FollowingController : MonoBehaviour
+{
+
+    public GameObject Target;
+    public float Height = 45f;
+    public float SmoothTime = 0.3f;
+    public float RotationSpped = 5f;
+
+    Vector3 positionVelocity = Vector3.zero;
+
+    // Update is called once per frame
+    void Update()
+    {
+        updatePosition();
+    }
+
+    void updatePosition()
+    {
+        var target = new Vector3(Target.transform.position.x,
+            Target.transform.position.y + Height, Target.transform.position.z);
+
+        transform.position = Vector3.SmoothDamp(transform.position,
+            target, ref positionVelocity, SmoothTime);
+    }
+}
